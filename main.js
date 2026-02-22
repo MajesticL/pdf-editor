2535bc22e6751dd2463901ce0f1c4dd1a2fe7999
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-02-22 06:09:41 -0500

    Add commented out pseudocode

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 pdfjsLib.GlobalWorkerOptions.workerSrc ='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.min.mjs';
 
 const fabricCanvas = new fabric.Canvas('fabric-canvas', { idDrawingMode: false});
+
+
+// Add PDF Load
+
+
+// Add editing tools
+// addText
+// setTool
+
+
+
+// Add PDF Save
+
+// Load original pdf into pdf lib
+
+// Generates image from fabric.js canvas
+
+// Embed image  into the PDF
+
+// Draw image on top of original page
+
+// Save/Download
+
+
+// Helper for download
