b5ad1962a2eac0e1005837206bcd32aba5de6e26
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-02-22 06:03:21 -0500

    Initialize Fabric js canvas

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-pdfjsLib.GlobalWorkerOptions.workerSrc ='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.min.mjs'
+pdfjsLib.GlobalWorkerOptions.workerSrc ='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.min.mjs';
 
+const fabricCanvas = new fabric.Canvas('fabric-canvas', { idDrawingMode: false});
