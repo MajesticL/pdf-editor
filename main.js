2545bd07ab1986ad227d231f9f37d131d3eddc8d
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-02-22 10:53:25 -0500

    Add download function

--- main.js
+++ main.js
@@ -93,3 +93,12 @@ async function savePDF() {
 }
 
 // Helper for download
+function download(data, filename, type) {
+    const blob = new Blob([data], { type: type });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
